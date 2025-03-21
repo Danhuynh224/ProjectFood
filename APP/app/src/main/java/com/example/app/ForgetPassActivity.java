ccd43ac9862800a092e323f05cc9c089612eadbc
VuongQue <lapque.hcmute@gmail.com>
2025-03-21 09:51:45 +0700

    nohing

--- APP/app/src/main/java/com/example/app/ForgetPassActivity.java
+++ APP/app/src/main/java/com/example/app/ForgetPassActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class ForgetPassActivity extends AppCompatActivity {
     @Override
+    //Vương Lập Quế - 22110402
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgetpass);
